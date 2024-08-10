130d2ff0f562c070a24d9aa1c5e758363693eced
matheuswacheski <125933774+matheuswacheski@users.noreply.github.com>
2024-08-10 12:43:16 -0300

    mudanças ladrilho para php

--- manipular_ladrilho_inicio.php
+++ manipular_ladrilho_inicio.php
@@ -1,7 +1,17 @@
 <?php 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $pontuacao_inicio = $_POST["alternancia_tentativa_inicio"];
 
-    echo "$pontuacao_inicio";
+    if(isset($_POST["alternancia_tentativa_inicio_5"])){
+        $pontuacao_ladrilho_inicio = $_POST["alternancia_tentativa_inicio_5"];
+    }else if(isset($_POST["alternancia_tentativa_inicio_3"])){
+        $pontuacao_ladrilho_inicio = $_POST["alternancia_tentativa_inicio_3"];
+    }else if(isset($_POST["alternancia_tentativa_inicio_1"])){
+        $pontuacao_ladrilho_inicio = $_POST["alternancia_tentativa_inicio_1"];
+    }else{
+        $pontuacao_ladrilho_inicio = 0;
+    }
+
+    header("Location: pista_a.html");
+    exit();
 }
 ?>
